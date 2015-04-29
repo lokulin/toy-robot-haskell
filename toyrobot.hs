0d11a19e9f3e4fc072e204c8d20cb82a69110721
lokulin <lauchlin@lauchlin.com>
2015-04-30 00:18:12 +1000

    Playing with folding

--- toyrobot.hs
+++ toyrobot.hs
@@ -4,22 +4,26 @@ import ToyRobot.Robot
 
 import Data.Char
 
-robot :: Robot
-robot = Robot (Point 0 0) 0 Nothing
+theRobot :: Robot
+theRobot = Robot (Point 0 0) 0 (Just (Table (Point 0 0) (Point 4 4)))
 
-sendCommand :: String -> String
-sendCommand "MOVE" = "MOVING"
-sendCommand "LEFT" = "LEFTING"
-sendCommand "RIGHT" = "RIGHTING"
-sendCommand "PLACE" = "PLACING"
-sendCommand "REPORTING" = "REPORTING"
-sendCommand x = ""
+sendCommand :: Robot -> String -> Robot
+sendCommand robot "MOVE" = move robot
+sendCommand robot "LEFT" = left robot
+sendCommand robot "RIGHT" = right robot
+sendCommand robot "REPORT" = robot
+sendCommand robot _ = robot
 
 execute :: String -> String
-execute = unlines . map (\line -> sendCommand line) . lines
+execute = unlines . filter (\return -> length return > 0 ) . map (\line -> show (sendCommand theRobot line)) . lines
 
 report :: String -> IO()
 report string = putStrLn string
 
 main :: IO()
-main = interact execute
+-- main = print (botit ["MOVE","RIGHT","MOVE","LEFT","REPORT"])
+-- main = interact execute
+main = do
+  s <- getContents
+  let r = show (foldl(\robit command -> sendCommand robit command) theRobot (lines s))
+  print r
