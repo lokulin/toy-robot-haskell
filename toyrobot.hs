297ea2683f3021b2cc233a2b79c3571d2a6f8873
lokulin <lauchlin@lauchlin.com>
2015-04-29 22:04:26 +1000

    WIP: started adding input functionality

--- toyrobot.hs
+++ toyrobot.hs
@@ -2,8 +2,24 @@ import ToyRobot.Point
 import ToyRobot.Table
 import ToyRobot.Robot
 
+import Data.Char
+
 robot :: Robot
 robot = Robot (Point 0 0) 0 Nothing
 
+sendCommand :: String -> String
+sendCommand "MOVE" = "MOVING"
+sendCommand "LEFT" = "LEFTING"
+sendCommand "RIGHT" = "RIGHTING"
+sendCommand "PLACE" = "PLACING"
+sendCommand "REPORTING" = "REPORTING"
+sendCommand x = ""
+
+execute :: String -> String
+execute = unlines . map (\line -> sendCommand line) . lines
+
+report :: String -> IO()
+report string = putStrLn string
+
 main :: IO()
-main = print robot
+main = interact execute
